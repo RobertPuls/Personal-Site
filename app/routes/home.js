921d8f0dbaefc06cc26e9ae5af4e5300ba1c126f
Robert Puls <rpu1s@sbcglobal.net>
2017-09-02 04:37:26 -0600

    starts skill board

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model() {
+    return ["skill1", "skill2", "skill3", "skill4", "skill1", "skill2", "skill3", "skill4"];
+  }
 });
