d77663ff510b203832420213b5bf892d326003e0
Robert Puls <rpu1s@sbcglobal.net>
2017-09-06 14:27:35 -0600

    postioning

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ["Ember.js", "Express", "PostgreSQL", "Node.js", "Javascript / Jquery", "HTML / CSS"];
+    return ["Ember.js", "Express", "PostgreSQL", "Node.js", "Javascript / JQuery", "HTML / CSS"];
   }
 });
