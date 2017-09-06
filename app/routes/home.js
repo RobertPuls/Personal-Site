6e2a6e514aeea2b14c5465776afcba10873acdb4
Robert Puls <rpu1s@sbcglobal.net>
2017-09-06 13:02:44 -0600

    changing trees

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return ["skill1", "skill2", "skill3", "skill4", "skill1", "skill2", "skill3", "skill4"];
+    return ["Ember.js", "Express", "PostgreSQL", "Node.js", "Javascript / Jquery", "HTML / CSS"];
   }
 });
